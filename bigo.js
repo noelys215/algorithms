bec8b8bf8cda3b768f9e3818ac14f78cbb3281a1
Henry B <noelys215@gmail.com>
2022-06-22 21:30:20 -0400

    nemo practice

--- bigo.js
+++ bigo.js
@@ -1,1 +1,25 @@
-console.log('hi');
+const fish = ['dory', 'bruce', 'marlin', 'nemo'];
+const nemo = ['nemo'];
+const everyone = [
+	'dory',
+	'bruce',
+	'marlin',
+	'nemo',
+	'gill',
+	'bloat',
+	'nigel',
+	'squirt',
+	'darla',
+	'hank',
+];
+const large = new Array(100).fill('nemo');
+
+function findNemo(array) {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === 'nemo') {
+			console.log('Found Nemo');
+		}
+	}
+}
+
+findNemo(large);
