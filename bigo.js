1f0ab24ab38cf3eef0df0615d7fd67b0c94f72c6
Henry B <noelys215@gmail.com>
2022-06-24 21:49:01 -0400

    added interview example

--- bigo.js
+++ bigo.js
@@ -63,4 +63,89 @@ function pairs(boxes) {
 	boxes.forEach((box) => boxes.forEach((secondBox) => console.log(box, secondBox)));
 }
 
-pairs(boxes);
+// pairs(boxes);
+
+// Space Complexity
+
+function boo(n) {
+	for (let i = 0; i < n.length; i++) {
+		console.log('boooo');
+	}
+}
+
+boo([1, 2, 3, 4, 5]); // O(1)
+
+function arrayOfHiNTimes(n) {
+	let hiArray = [];
+	for (let i = 0; i < n; i++) {
+		hiArray[i] = 'hi';
+	}
+	console.log(hiArray);
+}
+
+arrayOfHiNTimes(6); //O(n)
+
+const array = ['hi', 'bye', 'see ya'];
+
+//Interview Example
+
+// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
+//For Example:
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
+//should return false.
+//-----------
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+//should return true.
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+function containsCommonItem(arr1, arr2) {
+	for (let i = 0; i < arr1.length; i++) {
+		for (let j = 0; j < arr2.length; j++) {
+			if (arr1[i] === arr2[j]) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+//O(a*b)
+//O(1) - Space Complexity
+
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'a'];
+
+function containsCommonItem2(arr1, arr2) {
+	// loop through first array and create object where properties === items in the array
+	// can we assume always 2 params?
+
+	let map = {};
+	for (let i = 0; i < arr1.length; i++) {
+		if (!map[arr1[i]]) {
+			const item = arr1[i];
+			map[item] = true;
+		}
+	}
+	// loop through second array and check if item in second array exists on created object.
+	for (let j = 0; j < arr2.length; j++) {
+		if (map[arr2[j]]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+//O(a + b) Time Complexity
+//O(a) Space Complexity
+
+// containsCommonItem2(array1, array2)
+
+function containsCommonItem3(arr1, arr2) {
+	return arr1.some((item) => arr2.includes(item));
+}
+
+containsCommonItem(array1, array2);
+containsCommonItem2(array1, array2);
+containsCommonItem3(array1, array2);
