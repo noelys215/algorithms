238fe73b301f555cbaa6d94ba74c8ed6140d8167
Henry B <noelys215@gmail.com>
2022-06-25 19:45:54 -0400

    added html and created array structure

--- arrays.js
+++ arrays.js
@@ -10,4 +10,52 @@ strings.unshift('x'); //O(n)
 //splice - delete
 strings.splice(2, 0, 'alien'); //O(n)
 
-console.log(strings);
+//Static Arrays vs Dynamic Arrays
+let a = 'a';
+let b = a;
+
+a === 'c';
+
+//
+class MyArray {
+	constructor() {
+		this.length = 0;
+		this.data = {};
+	}
+
+	get(index) {
+		return this.data[index];
+	}
+
+	push(item) {
+		this.data[this.length] = item;
+		this.length++;
+		return this.length;
+	}
+	pop() {
+		const lastItem = this.data[this.length - 1];
+		delete this.data[this.length - 1];
+		this.length--;
+		return lastItem;
+	}
+
+	delete(index) {
+		const item = this.data[index];
+		this.shiftItems(index);
+	}
+	shiftItems(index) {
+		for (let i = index; i < this.length; i++) {
+			this.data[i] = this.data[i + 1];
+		}
+		delete this.data[this.length - 1];
+		this.length--;
+	}
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('bye');
+newArray.push('!');
+newArray.delete(1);
+
+console.log(newArray);
