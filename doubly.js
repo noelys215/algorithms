c73cde7ca181db7c0dd79fccd5682683d1ff092a
Henry B <noelys215@gmail.com>
2022-06-30 11:43:17 -0400

    added linked list examples

---
+++ doubly.js
@@ -0,0 +1,106 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class LinkedList {
+	constructor(value) {
+		this.head = {
+			value: value,
+			next: null,
+			prev: null,
+		};
+		this.tail = this.head;
+		this.length = 1;
+	}
+	append(value) {
+		const newNode = new Node(value);
+		newNode.prev = this.tail;
+		this.tail.next = newNode;
+		this.tail = newNode;
+		this.length++;
+		return this;
+	}
+	prepend(value) {
+		const newNode = new Node(value);
+		newNode.next = this.head;
+		this.head.prev = newNode;
+		this.head = newNode;
+		this.length++;
+		return this;
+	}
+	printList() {
+		const array = [];
+		let currentNode = this.head;
+		while (currentNode !== null) {
+			array.push(currentNode.value);
+			currentNode = currentNode.next;
+		}
+		return array;
+	}
+	insert(index, value) {
+		if (index >= this.length) {
+			return this.append(value);
+		}
+
+		const newNode = new Node(value);
+		const leader = this.traverseToIndex(index - 1);
+		const follower = leader.next;
+		leader.next = newNode;
+		newNode.prev = leader;
+		newNode.next = follower;
+		follower.prev = newNode;
+		this.length++;
+		return this.printList();
+	}
+
+	traverseToIndex(index) {
+		let counter = 0;
+		let currentNode = this.head;
+		while (counter !== index) {
+			currentNode = currentNode.next;
+			counter++;
+		}
+		return currentNode;
+	}
+
+	remove(index) {
+		const leader = this.traverseToIndex(index - 1);
+		const unwantedNode = leader.next;
+		leader.next = unwantedNode.next;
+		this.length--;
+		return this.printList();
+	}
+
+	reverse() {
+		if (!this.head.next) {
+			return this.head;
+		}
+
+		let first = this.head;
+		this.tail = this.head;
+		let second = first.next;
+		while (second) {
+			const temp = second.next;
+			second.next = first;
+			first = second;
+			second = temp;
+		}
+		this.head.next = null;
+		this.head = first;
+		return this;
+	}
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(1, 99);
+myLinkedList.reverse();
+console.log(myLinkedList);
+
+console.log(myLinkedList.printList());
