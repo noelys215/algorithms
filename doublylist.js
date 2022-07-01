20d64cb1ba0140b797d670991f048239f7652a97
Henry B <noelys215@gmail.com>
2022-06-30 20:18:06 -0400

    added doubly linked lists examples

--- doublylist.js
+++ doublylist.js
@@ -49,10 +49,11 @@ class DoublyLinkedList {
 		if (this.length === 1) {
 			this.head = null;
 			this.tail = null;
+		} else {
+			this.head = oldHead.next;
+			this.head.prev = null;
+			oldHead.next = null;
 		}
-		this.head = oldHead.next;
-		this.head.prev = null;
-		oldHead.next = null;
 		this.length--;
 		return oldHead;
 	}
