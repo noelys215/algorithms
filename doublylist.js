1db0dbc5e60c7bb6a87eedaff5f7c9043f9cdcf2
Henry B <noelys215@gmail.com>
2022-06-30 20:16:58 -0400

    added doubly linked lists examples

---
+++ doublylist.js
@@ -0,0 +1,65 @@
+class Node {
+	constructor(val) {
+		this.value = val;
+		this.next = null;
+		this.null = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	push(val) {
+		const newNode = new Node(val);
+
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+
+	pop() {
+		if (!this.head) return undefined;
+		let poppedNode = this.tail;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.tail = poppedNode.prev;
+			this.tail.next = null;
+			poppedNode.prev = null;
+		}
+		this.length--;
+		return poppedNode;
+	}
+
+	shift() {
+		if (this.length === 0) return undefined;
+		let oldHead = this.head;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		}
+		this.head = oldHead.next;
+		this.head.prev = null;
+		oldHead.next = null;
+		this.length--;
+		return oldHead;
+	}
+}
+
+const list = new DoublyLinkedList();
+
+list.push(100);
+list.push(200);
+console.log(list);
