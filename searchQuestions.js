c76a8ced231b98d800bc15dce49fa57cc4043bd7
Henry B <noelys215@gmail.com>
2022-07-09 23:26:20 -0400

    adding BDF and DFS

---
+++ searchQuestions.js
@@ -0,0 +1,27 @@
+/* Breadth First Search/Traversal */
+// + Shortest Path Closer Nodes
+// - More Memory Used
+
+/* Depth    First Search/Traversal */
+// + Less Memory Used/Check if Path Exist from Source Node to a Target Node
+// - Can Get Slow
+
+/* Exercise/Example Questions */
+
+//If you know a solution is not far from the root of the tree:
+/* BFS */
+
+//If the tree is very deep and solutions are rare:
+/* BFS (DFS will take a longer time) */
+
+//If the tree is very wide:
+/* DFS (BFS will need too much memory) */
+
+//If solutions are frequent but located deep in the tree:
+/* DFS */
+
+//Determining whether a path exists between two nodes:
+/* DFS */
+
+//Finding the shortest path:
+/* BFS */
